10c10ad07a231e6466a4e0bca9ea6159624cab3d
AcerStructor <acerstructordev@gmail.com>
2024-03-24 21:55:59 +0800

    Edit formatting of initScene.c and added comments

--- src/initScene.c
+++ src/initScene.c
@@ -18,15 +18,18 @@ static void destroyScene(); // temporary
 
 void initScene()
 {
+    /* --- Assign Delegates --- */
     app.delegate.update     = update;
     app.delegate.render     = render;
     app.delegate.destroy    = destroyScene;
 
+    /* --- Allocate Game Objects --- */
     entity = (Entity*)malloc(sizeof(Entity));
 
-    entity->sprite = load_texture("res/sprites/placeholder.png");
-    entity->pos.x = WINDOW_WIDTH / 2;
-    entity->pos.y = WINDOW_HEIGHT / 2;
+    /* --- Load Sprites --- */
+    entity->sprite  = load_texture("res/sprites/placeholder.png");
+    entity->pos.x   = WINDOW_WIDTH  / 2;
+    entity->pos.y   = WINDOW_HEIGHT / 2;
 }
 
 static void update(float delta)
