61b02226a9fbe1a331951d3f921665aa96cc4f74
AcerStructor <acerstructordev@gmail.com>
2024-03-24 22:15:58 +0800

    Added state.h

---
+++ src/states.h
@@ -0,0 +1,17 @@
+/**
+ *
+ * FIle Name: states.h
+ *
+ * Description:
+ *      This header file consists of program states
+ *      in order to manage which state the program is in.
+ *      In short, it is the main implementation for
+ *      enums
+ */
+
+#ifndef _STATES_H_
+#define _STATES_H_
+
+// TODO
+
+#endif // _STATES_H_
